5ccb3a6a5f11018c84099a2c2b786f906bede102
Kien Nguyen <kennygunie@gmail.com>
2015-08-26 00:34:26 +0200

    helper methods for: UIViewController, UIStoryboard

---
+++ Pod/Classes/UIStoryboard+NiosKit.m
@@ -0,0 +1,17 @@
+//
+//  UIStoryboard+NiosKit.m
+//  Pods
+//
+//  Created by Kien NGUYEN on 26/08/2015.
+//
+//
+
+#import "UIStoryboard+NiosKit.h"
+
+@implementation UIStoryboard (NiosKit)
+
++ (instancetype)mainStoryboard {
+    return [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+}
+
+@end
